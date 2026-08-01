a9bce3632f6216b59c3e1d8c3b76e3a995d6f4d4
Parmar Karan <orazonwellness@gmail.com>
2026-08-01 12:18:50 +0530

    Implement mobile menu toggle and close functionality
    Added mobile menu functionality with event listeners for toggling and closing the menu.

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const menuToggle = document.getElementById("menu-toggle");
+/*=========================================
+        ORAZON WELLNESS
+=========================================*/
 
+// Mobile Menu
+
+const menuToggle = document.getElementById("menu-toggle");
 const navbar = document.querySelector(".navbar");
+
+if(menuToggle){
 
 menuToggle.addEventListener("click",()=>{
 
 navbar.classList.toggle("active");
+
+});
+
+}
+
+// Close Menu After Click
+
+const navLinks=document.querySelectorAll(".nav-links a");
+
+navLinks.forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+navbar.classList.remove("active");
+
+});
 
 });
